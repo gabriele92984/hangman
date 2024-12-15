b8ca63398fb77230e9d74a595a9b10f976d374a6
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-12-15 15:29:40 +0100

    Create basic class Hangman structure

--- hangman.rb
+++ hangman.rb
@@ -1,57 +1,84 @@
-# Procedural Version
-puts 'Welcome to Hangman game!'
+# # Procedural Version
+# puts 'Welcome to Hangman game!'
 
-def load_dictionary
-  selected_words = []  # Define selected_words within the method
-  begin
-    File.readlines('data/google-10000-english-no-swears.txt').each do |word|
-      selected_words << word.strip if word.size.between?(5, 12)
-    end
-  rescue Errno::ENOENT
-    puts 'Error: Dictionary file not found.'
-    exit 1
-  selected_words  # Return the selected words
-  end
-end
+# def load_dictionary
+#   selected_words = []  # Define selected_words within the method
+#   begin
+#     File.readlines('data/google-10000-english-no-swears.txt').each do |word|
+#       selected_words << word.strip if word.size.between?(5, 12)
+#     end
+#   rescue Errno::ENOENT
+#     puts 'Error: Dictionary file not found.'
+#     exit 1
+#   end
+#   selected_words  # Return the selected words
+# end
 
-# words_list = load_dictionary  # Capture returned value
-secret_word = load_dictionary.sample
-puts "The secret word chosen is: #{secret_word}" # For debugging
+# # words_list = load_dictionary  # Capture returned value
+# secret_word = load_dictionary.sample
+# puts "The secret word chosen is: #{secret_word}" # For debugging
 
-incorrect_letters = []
-board = ['_'] * secret_word.size
-puts board.join(' ')
+# incorrect_letters = []
+# board = ['_'] * secret_word.size
+# puts board.join(' ')
 
-lives = 6
+# lives = 6
 
-while lives > 0 && board.include?('_')
-  print "\nYou have #{lives} lives left. Please, guess a letter: "
-  guess = gets.chomp 
-  # puts "Your guess was: #{guess}"
+# while lives > 0 && board.include?('_')
+#   print "\nYou have #{lives} lives left. Please, guess a letter: "
+#   guess = gets.chomp 
+#   # puts "Your guess was: #{guess}"
 
-  if secret_word.include?(guess)
-    # If any of the letters in secret_word match then update board
-    # at the location of the matching letters to reveal the guessed word.
-    secret_word.chars.each_with_index do |char, index|
-      if char.downcase == guess.downcase
-        board[index] = char
-      end
-    end
-  else
-    lives -= 1
-    incorrect_letters << guess
-    puts "The word did not include: #{guess}"
-    puts "Incorrect letters chosen: #{incorrect_letters}"
-  end
+#   if secret_word.include?(guess)
+#     # If any of the letters in secret_word match then update board
+#     # at the location of the matching letters to reveal the guessed word.
+#     secret_word.chars.each_with_index do |char, index|
+#       if char.downcase == guess.downcase
+#         board[index] = char
+#       end
+#     end
+#   else
+#     lives -= 1
+#     incorrect_letters << guess
+#     puts "The word did not include: #{guess}"
+#     puts "Incorrect letters chosen: #{incorrect_letters}"
+#   end
 
-  puts board.join(' ')
-end
+#   puts board.join(' ')
+# end
 
-if board.join('') == secret_word
-  puts "Congrats, you won!"
-else
-  puts "Sorry, you lose... The secret word was: #{secret_word}"
-end
+# if board.join('') == secret_word
+#   puts "Congrats, you won!"
+# else
+#   puts "Sorry, you lose... The secret word was: #{secret_word}"
+# end
 
 # Object-Oriented Version
 
+class Hangman
+  def initialize
+    @lives = 6
+    @selected_words = []
+    @secret_word = words_list.sample
+  end
+
+  def words_list
+    begin
+      File.readlines('data/google-10000-english-no-swears.txt').each do |word|
+        @selected_words << word.strip if word.size.between?(5, 12)
+      end
+    rescue Errno::ENOENT
+      puts 'Error: Dictionary file not found.'
+      exit 1
+    end
+    @selected_words  # Return the selected words
+  end
+
+  def play
+    puts 'Welcome to Hangman!'
+    puts "The secret word chosen is: #{@secret_word}"
+  end
+end
+
+game = Hangman.new
+game.play

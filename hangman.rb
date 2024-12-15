f59e307cc1d0922fbf6c7ec7234ded144813dd40
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-12-15 15:12:56 +0100

    Add exception handling for dictionary file

--- hangman.rb
+++ hangman.rb
@@ -1,11 +1,17 @@
+# Procedural Version
 puts 'Welcome to Hangman game!'
 
 def load_dictionary
   selected_words = []  # Define selected_words within the method
+  begin
     File.readlines('data/google-10000-english-no-swears.txt').each do |word|
       selected_words << word.strip if word.size.between?(5, 12)
     end
+  rescue Errno::ENOENT
+    puts 'Error: Dictionary file not found.'
+    exit 1
   selected_words  # Return the selected words
+  end
 end
 
 # words_list = load_dictionary  # Capture returned value
@@ -20,7 +26,7 @@ lives = 6
 
 while lives > 0 && board.include?('_')
   print "\nYou have #{lives} lives left. Please, guess a letter: "
-  guess = gets.chomp
+  guess = gets.chomp 
   # puts "Your guess was: #{guess}"
 
   if secret_word.include?(guess)
@@ -46,3 +52,6 @@ if board.join('') == secret_word
 else
   puts "Sorry, you lose... The secret word was: #{secret_word}"
 end
+
+# Object-Oriented Version
+
